5d4a846da1e631b70297981243b4f73e8a965e18
IvNik <ivnik@IvNiks-MacBook-Pro.local>
2024-04-22 18:37:50 +0300

    open and close  sidebar

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,28 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
 import styles from './ArticleParamsForm.module.scss';
 
 type ArticleParamsFormProps = {
   isOpen: boolean;
-	handleToggleSidebar: () => void;
+  handleToggleSidebar: () => void;
 };
 
-export const ArticleParamsForm = ({ isOpen, handleToggleSidebar }: ArticleParamsFormProps) => {
-	 // Состояние для отслеживания открытия/закрытия панели
-	const [isFormOpen, setIsFormOpen] = useState(false);
+export const ArticleParamsForm: React.FC<ArticleParamsFormProps> = ({ isOpen, handleToggleSidebar }) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const arrowButtonRef = useRef<HTMLDivElement>(null);
 
-	 // Функция для открытия/закрытия панели
-	const toggleForm = () => {
-		setIsFormOpen(!isFormOpen);
-	};
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (containerRef.current && !containerRef.current.contains(event.target as Node) && !arrowButtonRef.current?.contains(event.target as Node)) {
+        if (isOpen) {
+          handleToggleSidebar();
+        }
+      }
+    };
 
-return (
+    document.addEventListener('click', handleClickOutside);
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isOpen, handleToggleSidebar]);
+
+  const toggleForm = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  return (
     <>
-      <ArrowButton onClick={handleToggleSidebar} />
-      {/* Добавляем класс styles.open, если панель открыта */}
-      <aside className={`${styles.container} ${isOpen ? styles.container_open : ''}`}>
+      <div ref={arrowButtonRef}>
+        <ArrowButton onClick={handleToggleSidebar} />
+      </div>
+      <aside className={`${styles.container} ${isOpen ? styles.container_open : ''}`} ref={containerRef}>
         <form className={styles.form}>
           <div className={styles.bottomContainer}>
             <Button title='Сбросить' type='reset' />
